742aa086cabdd4b99154a5893ff4f7beb6c33429
Chris Heald <raythfalstar@gmail.com>
2016-07-21 11:36:46 +0100

    React Classes created

--- src/app.js
+++ src/app.js
@@ -4,8 +4,59 @@ const ReactDOM = require('react-dom');
 const App = React.createClass({
   render: function () {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h1 className="">Hello World</h1>
+        <ListBoard />
+        <NavBar />
+        <Menu />
+      </div>
     );
+  }
+});
+
+const ListBoard = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h2>This is the board for lists</h2>
+        <List />
+      </div>
+    )
+  }
+});
+
+const List = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h3>This is a list</h3>
+        <Card />
+      </div>
+    )
+  }
+});
+
+const Card = React.createClass({
+  render: function () {
+    return (
+      <h4>This is a card</h4>
+    )
+  }
+});
+
+const NavBar = React.createClass({
+  render: function () {
+    return (
+      <div>Navbar!</div>
+    )
+  }
+});
+
+const Menu = React.createClass({
+  render: function () {
+    return (
+      <div>I'm a menu! :D</div>
+    )
   }
 });
 
